a64ccdebd24891f30e4f7aebe1a63a164d5b60d6
brunomilassin <brunomilassin@gmail.com>
2026-04-16 16:18:14 +0200

    Add MusicGenreRecognition.py

---
+++ MusicGenreRecognition.py
@@ -0,0 +1,15 @@
+#This is Bartal Baranyi's and Bruno Milassin's work
+#this topic is about music genre recognition using machine learning techniques. 
+# The code will likely involve data preprocessing, feature extraction, and the 
+# implementation of a machine learning model to classify music genres based on audio features.
+
+#the code will likely contain elements from these libraries:
+import librosa # audio loading and feature extraction
+import numpy # numerical arrays
+import pandas # dataframes
+import scikit-learn # ML models, train/test split, metrics
+import matplotlib # plots
+import seaborn 
+
+
+print("Hello World! This is the first commit line, I hope you like it!")
